5db7f3526c80b0a09528920d94d83461573738a8
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-11 19:59:57 +0000

    Add booking template html file

--- booking/admin.py
+++ booking/admin.py
@@ -4,4 +4,8 @@ from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-admin.site.register(booking)
+@admin.register(booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'booking_date', 'booking_time', 'number_of_people')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('booking_date',)
