fa8500a486ce5900409dfbdcecacab6711917474
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-11 19:26:09 +0000

    Add model for booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class booking(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    comments = models.TextField(blank=True)
+    number_of_people = models.PositiveIntegerField()
