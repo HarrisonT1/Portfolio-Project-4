98e15f2bfc551161c1f3481183683f4384250a35
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-12 23:41:24 +0000

    Add duration field and function to calculate time requested to meet user story

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
 # Create your models here.
 
@@ -11,8 +12,17 @@ class Booking(models.Model):
     email = models.EmailField()
     booking_date = models.DateField()
     booking_time = models.TimeField()
+    duration = models.DurationField(default=timedelta(hours=1), help_text="Length of stay")
     comments = models.TextField(blank=True)
     number_of_people = models.PositiveIntegerField()
+
+    def CalcTime(self, *args, **kwargs):
+        if self.duration:
+            start_datetime = datetime.combine(self.booking_date, self.booking_time)
+            end_datetime = start_datetime + self.duration
+            self.end_time = end_datetime.time()
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} booked for {self.booking_time} on {self.booking_date}"
