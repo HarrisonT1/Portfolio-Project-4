ad3caa6e8cd289e89da224183990d89f33ad649d
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-13 20:58:40 +0000

    Allow users to view specific bookings through the booking id

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path  # import path, similar to project's urls.py
 from . import views  # import views.py from the current directory
 
 urlpatterns = [
-    path('profile/', views.Profile, name='profile'),
     path('profile/bookings/', views.BookingList, name='booking_list'),
+    path('profile/bookings/<booking_id>', views.Profile, name='profile'),
 ]
--- profile_app/views.py
+++ profile_app/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -14,3 +15,9 @@ def Profile(request):
 def BookingList(request):
     bookings = Booking.objects.all().filter(user=request.user)
     return render(request, 'profile_app/booking_list.html', {'bookings': bookings})
+
+
+@login_required
+def BookingDetails(request, booking_id):
+    booking = get_object_or_404(Booking, booking_id=booking_id, user=request.user)
+    return render(request, 'profile_app/booking_details.html', {'booking': booking})
