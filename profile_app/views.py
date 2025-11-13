ac5482db72b0b5dbde01b2b2c7f30227168fc9cf
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-13 21:50:32 +0000

    Add view to allow users to edit bookings

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse
 from booking.models import Booking
+from booking.forms import BookingForm
 
 # Create your views here.
 
@@ -24,3 +26,26 @@ def BookingCancel(request, booking_id):
     booking = get_object_or_404(Booking, booking_id=booking_id, user=request.user)
     booking.delete()
     return redirect('booking_list')
+
+
+@login_required
+def BookingEdit(request, booking_id):
+    booking = get_object_or_404(Booking, booking_id=booking_id, user=request.user)
+
+    form = BookingForm(request.POST or None, instance=booking)
+
+    if request.method == 'POST':
+        if form.is_valid():
+
+            # Add code to reset approval from staff member once staff app is created
+
+            messages.success(request, "Your booking has successfully been updated and is awaiting staff approval!")
+            return redirect('booking_list')
+        else:
+            messages.error(request, "Error updating your booking. Please review your changes and try again.")
+
+    context = {
+        'form': form,
+        'booking': booking
+    }
+    return render(request, 'profile_app/booking_edit', context)
