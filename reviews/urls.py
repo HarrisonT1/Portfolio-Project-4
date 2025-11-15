a43100345ae8ffa245e748c00830fdb0ad525cb8
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-15 16:44:47 +0000

    Add success page to urlpattern

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views  # import views.py from the current directory
 
 urlpatterns = [
     path('review/create/', views.CreateReview, name='create_review'),
+    path('review/success/<int:review_id>/', views.ReviewSuccess, name='review_success'),
 ]
