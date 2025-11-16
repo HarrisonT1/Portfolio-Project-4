b28ed1856bd75f23e35d167894cd658d20987826
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-16 18:40:15 +0000

    Create ApproveBooking view

--- staff/views.py
+++ staff/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from booking.models import Booking
 
 # Create your views here.
 
 
+def ApproveBooking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.aprroved = 'approved'
+    booking.save()
+    return redirect('staff_booking_list')
