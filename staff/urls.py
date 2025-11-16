f6f27ef5fa86d6cbe7faf73e4908171c56f4f2fd
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-16 22:12:54 +0000

    Change url name

--- staff/urls.py
+++ staff/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('staff/dashboard/', views.StaffBookingList, name='staff_dashboard'),
+    path('staff/dashboard/booking_list/', views.StaffBookingList, name='staff_booking_list'),
     path('staff/dashboard/booking_list/approve/<int:booking_id>/', views.ApproveBooking, name='approve_booking'),
 ]
