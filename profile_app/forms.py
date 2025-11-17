6308da68abc7a9d5edefe0a7e79114184378b4b3
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-17 18:32:16 +0000

    Create edit_profile.html, add form to it, fix capitalisation for meta in form

--- profile_app/forms.py
+++ profile_app/forms.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class EditProfileForm(forms.ModelForm):
-    class meta:
+    class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
