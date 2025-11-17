2e3ce589b0337710c27fcc32ec6cd46d27c3c79a
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-17 20:46:22 +0000

    Add review_list url and basic html template to show reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path  # import path, similar to project's urls.py
 from . import views  # import views.py from the current directory
 
 urlpatterns = [
+    path('reviews/', views.review_list, name='review_list'),
     path('review/create/', views.CreateReview, name='create_review'),
     path('review/success/<int:review_id>/', views.ReviewSuccess, name='review_success'),
 ]
