679bbd479c946404185e5d8abc2b8de692a01220
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-17 23:25:56 +0000

    Add to admin.py file in reviews, to make admin panel easier to use

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,5 +3,14 @@ from .models import Review, Comment
 
 # Register your models here.
 
-admin.site.register(Review)
-admin.site.register(Comment)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'star_rating', 'approved', 'created_at')
+    list_filter = ('approved', 'created_at', 'star_rating')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'review', 'id')
+    search_fields = ('comment',)
+    list_filter = ('created_at',)
