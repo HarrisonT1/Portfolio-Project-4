4b4bca6769bb7a73e3ed30b99fb0f05f81906d35
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-22 19:25:17 +0000

    Add DTL to display reviews and add some styling to colour

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from reviews.models import Review
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+
+    reviews = Review.objects.filter(approved='approved').order_by('-star_rating')
+
+    return render(request, "home/index.html", {'reviews': reviews})
--- reviews/utils.py
+++
@@ -1,5 +0,0 @@
-from .models import Review
-
-
-def all_reviews():
-    return Review.objects.filter(approved='approved').order_by('-star_rating')
