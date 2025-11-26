ec3e09c44aabb4b54253778a834ddd172c465600
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-26 12:11:09 +0000

    Change js popup to a bootstrap modal

--- static/js/button_confirmation.js
+++ static/js/button_confirmation.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const cancelButtons = document.querySelectorAll(".cancel-btn")
+    const modal = new bootstrap.Modal(document.getElementById('cancelConfirmation'))
+    const confirmBtn = document.getElementById('cancelConfirm')
 
     cancelButtons.forEach(button => {
         button.addEventListener("click", function(event) {
-            const confirmation = confirm("Are you sure you want to delete this, this action is irreversible");
-            if(!confirmation) {
-                event.preventDefault();
-            }
+            event.preventDefault();
+            const href = this.getAttribute("href");
+            confirmBtn.setAttribute("href", href)
+            modal.show()
         })
     })
 })
