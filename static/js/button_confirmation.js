8d7589bea94d601822b3aeaccd44889eac2e90b6
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-26 11:51:06 +0000

    Add confirmation to canceling order

--- static/js/button_confirmation.js
+++ static/js/button_confirmation.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+document.addEventListener("DOMContentLoaded", function() {
+    const cancelButtons = document.querySelectorAll(".cancel-btn")
+
+    cancelButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            const confirmation = confirm("Are you sure you want to delete this, this action is irreversible");
+            if(!confirmation) {
+                event.preventDefault();
+            }
+        })
+    })
+})
