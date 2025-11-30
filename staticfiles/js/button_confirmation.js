cdf66ac48ee8d0b0921ce964cdde476af7f1d2cf
HarrisonT1 <harrisontownend@hotmail.com>
2025-11-30 10:44:46 +0000

    Collect static files

--- staticfiles/js/button_confirmation.js
+++ staticfiles/js/button_confirmation.js
@@ -1,14 +1,16 @@
+/* global bootstrap */
+
 document.addEventListener("DOMContentLoaded", function() {
-    const cancelButtons = document.querySelectorAll(".cancel-btn")
-    const modal = new bootstrap.Modal(document.getElementById('cancelConfirmation'))
-    const confirmBtn = document.getElementById('cancelConfirm')
+    const cancelButtons = document.querySelectorAll(".cancel-btn");
+    const modal = new bootstrap.Modal(document.getElementById('cancelConfirmation'));
+    const confirmBtn = document.getElementById('cancelConfirm');
 
     cancelButtons.forEach(button => {
         button.addEventListener("click", function(event) {
             event.preventDefault();
             const href = this.getAttribute("href");
-            confirmBtn.setAttribute("href", href)
-            modal.show()
-        })
-    })
-})
+            confirmBtn.setAttribute("href", href);
+            modal.show();
+        });
+    });
+});
--- staticfiles/js/messages-disappear.js
+++ staticfiles/js/messages-disappear.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const alerts = document.querySelectorAll('.alert');
     alerts.forEach(alert => {
         setTimeout(() => {
-            alert.classList.remove('show')
+            alert.classList.remove('show');
             setTimeout(() => {
                 alert.remove();
             }, 500);
